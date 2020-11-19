da4b16fc0f3d5466d50fa6353916cc8d51c1c9da
xLocalName <xLocalEmail@gmail.com>
2020-11-19 17:58:29 +0300

    update session.py

--- fixture/session.py
+++ fixture/session.py
@@ -17,6 +17,12 @@ class SessionHelper:
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
+        wd.find_element_by_name("username")
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.logout()
 
     def is_logged_in(self):
         wd = self.app.wd
@@ -28,12 +34,7 @@ class SessionHelper:
 
     def get_logged_user(self):
         wd = self.app.wd
-        return wd.find_element_by_css_selector('td.login-info-left span').text
-
-    def ensure_logout(self):
-        wd = self.app.wd
-        if self.is_logged_in():
-            self.logout()
+        return wd.find_element_by_css_selector("td.login-info-left span").text
 
     def ensure_login(self, username, password):
         wd = self.app.wd
